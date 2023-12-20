2d28546e5bf3a20527d0361a08a6fd389e12e113
parameswari.bala <parameswaribala@gmail.com>
2023-12-20 20:31:42 +0530

    updated

--- bankinglocalzeebeclient/src/main/java/com/virtusa/bankinglocalzeebeclient/configurations/JobConfiguration.java
+++ bankinglocalzeebeclient/src/main/java/com/virtusa/bankinglocalzeebeclient/configurations/JobConfiguration.java
@@ -7,6 +7,8 @@ import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -45,6 +47,31 @@ public class JobConfiguration {
 
     }
     
-    
+    @JobWorker(type = "checkAge",autoComplete = false)
+    public Map<String,Object> validateAge(final JobClient jobClient, ActivatedJob activatedJob){
+          
+    	Map<String,Object> response= activatedJob.getVariablesAsMap();
+    	LocalDate dob=LocalDate.parse(response.get("dob").toString());
+        log.info("The DOB ="+dob);
+        long age=ChronoUnit.YEARS.between(dob,LocalDate.now());
+        
+        response.put("age", age);
+        if(dob.isBefore(LocalDate.now())&&(age>=21)) {    	
+        	
+        	response.put("isAgeValid", true);
+        jobClient.newCompleteCommand(activatedJob.getKey())
+                .send().exceptionally(throwable -> {
+                   throw new RuntimeException("Exception due to non available job");
+                });
+         
+        }else {        
+        	response.put("isAgeValid", false);
+        jobClient.newCompleteCommand(activatedJob.getKey())
+                .send().exceptionally(throwable -> {
+                   throw new RuntimeException("Exception due to non available job");
+                });
+        }
+        return response;
+    }
     
 }
